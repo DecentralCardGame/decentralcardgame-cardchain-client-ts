740af8fa5b6fea31d4cf5d28a9472dc4a3545f0a
lxgr-linux <lxgr@protonmail.com>
2023-11-02 23:36:17 +0100

    this is the final straw

--- DecentralCardGame.cardchain.cardchain/types/cardchain/cardchain/aminomessages.js
+++ DecentralCardGame.cardchain.cardchain/types/cardchain/cardchain/aminomessages.js
@@ -8,16 +8,25 @@ export function createDecentralCardgameAminoConverters() {
     return {
         "/DecentralCardGame.cardchain.cardchain.MsgTransferCard": {
             aminoType: "cardchain/TransferCard",
-            toAmino: (msg) => (msg),
-            fromAmino: (msg) => (msg),
+            toAmino: (msg) => ({
+                creator: msg.creator,
+                card_id: msg.cardId,
+                receiver: msg.receiver
+            }),
+            fromAmino: (msg) => ({
+                creator: msg.creator,
+                cardId: msg.card_id,
+                receiver: msg.receiver
+            }),
         },
         "/DecentralCardGame.cardchain.cardchain.MsgRegisterForCouncil": {
             aminoType: "cardchain/RegisterForCouncil",
-            toAmino: (msg) => {
-                console.log("Hier hier, was da los?", msg);
-                return msg;
-            },
-            fromAmino: (msg) => (msg),
+            toAmino: (msg) => ({
+                creator: msg.creator
+            }),
+            fromAmino: (msg) => ({
+                creator: msg.creator
+            }),
         },
     };
 }
--- DecentralCardGame.cardchain.cardchain/types/cardchain/cardchain/aminomessages.ts
+++ DecentralCardGame.cardchain.cardchain/types/cardchain/cardchain/aminomessages.ts
@@ -1,52 +1,61 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { AminoMsg } from "@cosmjs/amino";
+import {AminoMsg} from "@cosmjs/amino";
 
 // eslint-disable-next-line import/no-cycle
-import { AminoConverter } from "@cosmjs/stargate";
-import {MsgRegisterForCouncil, MsgTransferCard } from "./tx";
+import {AminoConverter} from "@cosmjs/stargate";
+import {MsgRegisterForCouncil, MsgTransferCard} from "./tx";
 
 type AminoConverters = Record<string, AminoConverter>;
 
 /** A high level transaction of the coin module */
 export interface AminoMsgTransferCard extends AminoMsg {
-  readonly type: "cardchain/TransferCard";
-  readonly value: {
-    readonly creator: string;
-    readonly cardId: number;
-    readonly receiver: string;
-  };
+    readonly type: "cardchain/TransferCard";
+    readonly value: {
+        readonly creator: string;
+        readonly card_id: number;
+        readonly receiver: string;
+    };
 }
 
 export interface AminoMsgRegisterForCouncil extends AminoMsg {
-  readonly type: "cardchain/RegisterForCouncil";
-  readonly value: {
-    readonly creator: string;
-  };
+    readonly type: "cardchain/RegisterForCouncil";
+    readonly value: {
+        readonly creator: string;
+    };
 }
 
 export function isAminoMsgTransferCard(msg: AminoMsg): msg is AminoMsgTransferCard {
-  return msg.type === "cardchain/TransferCard";
+    return msg.type === "cardchain/TransferCard";
 }
 
 export function isAminoMsgRegisterForCouncil(msg: AminoMsg): msg is AminoMsgRegisterForCouncil {
-  return msg.type === "cardchain/RegisterForCouncil";
+    return msg.type === "cardchain/RegisterForCouncil";
 }
 
 
 export function createDecentralCardgameAminoConverters(): AminoConverters {
-  return {
-    "/DecentralCardGame.cardchain.cardchain.MsgTransferCard": {
-      aminoType: "cardchain/TransferCard",
-      toAmino: (msg: MsgTransferCard): AminoMsgTransferCard["value"] => (msg),
-      fromAmino: (msg: AminoMsgTransferCard["value"]): MsgTransferCard => (msg),
-    },
-    "/DecentralCardGame.cardchain.cardchain.MsgRegisterForCouncil": {
-      aminoType: "cardchain/RegisterForCouncil",
-      toAmino: (msg: MsgRegisterForCouncil): AminoMsgRegisterForCouncil["value"] => {
-          console.log("Hier hier, was da los?", msg)
-          return msg
+    return {
+        "/DecentralCardGame.cardchain.cardchain.MsgTransferCard": {
+            aminoType: "cardchain/TransferCard",
+            toAmino: (msg: MsgTransferCard): AminoMsgTransferCard["value"] => ({
+                creator: msg.creator,
+                card_id: msg.cardId,
+                receiver: msg.receiver
+            }),
+            fromAmino: (msg: AminoMsgTransferCard["value"]): MsgTransferCard => ({
+                creator: msg.creator,
+                cardId: msg.card_id,
+                receiver: msg.receiver
+            }),
         },
-      fromAmino: (msg: AminoMsgRegisterForCouncil["value"]): MsgRegisterForCouncil => (msg),
-    },
-  };
+        "/DecentralCardGame.cardchain.cardchain.MsgRegisterForCouncil": {
+            aminoType: "cardchain/RegisterForCouncil",
+            toAmino: (msg: MsgRegisterForCouncil): AminoMsgRegisterForCouncil["value"] => ({
+                creator: msg.creator
+            }),
+            fromAmino: (msg: AminoMsgRegisterForCouncil["value"]): MsgRegisterForCouncil => ({
+                creator: msg.creator
+            }),
+        },
+    };
 }
